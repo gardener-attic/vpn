7d6183ffec54da3b5a75aa62b74aabe2e1b7c428
Rafael Franzke <rafael.franzke@sap.com>
2018-05-07 16:20:57 +0200

    Merge openvpn-* directories into seed/shoot
    We do not want to double maintain two different implementations while only one is in use.

--- seed/network-connection.sh
+++ seed/network-connection.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #
-# Copyright (c) 2017 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+# Copyright (c) 2018 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,23 +20,15 @@ function log() {
 
 trap 'exit' TERM SIGINT
 
-if [[ -z "$IDENTITY_FILE" ]]; then
-  log "error: environment variable IDENTITY_FILE not set"
-  exit 1
-fi
-
-# copy identity file into another directory (it is not writeable, thus we cannot change the mode)
-mkdir -p /var/vpn-seed
-cp $IDENTITY_FILE /var/vpn-seed/id_rsa
-chmod 600 /var/vpn-seed/id_rsa
-
-service_network="${SERVICE_NETWORK:-100.64.0.0/13}"
-pod_network="${POD_NETWORK:-100.96.0.0/11}"
-node_network="${NODE_NETWORK:-10.250.0.0/16}"
 service_name="${SERVICE_NAME:-vpn-shoot}"
+openvpn_port="${OPENVPN_PORT:-1194}"
 
-failed_pings=0
-successful_ssh_connections_without_ping=0
+tcp_keepalive_time="${TCP_KEEPALIVE_TIME:-7200}"
+tcp_keepalive_intvl="${TCP_KEEPALIVE_INTVL:-75}"
+tcp_keepalive_probes="${TCP_KEEPALIVE_PROBES:-9}"
+tcp_retries2="${TCP_RETRIES2:-5}"
+
+curl_timeout_options="--connect-timeout 5 --max-time 5"
 
 function get_host() {
   if [[ -z "$MAIN_VPN_SEED" ]]; then
@@ -52,6 +44,7 @@ function identify_endpoint() {
   set +e
   BASIC_AUTH="admin:$(cat /srv/auth/basic_auth.csv | sed -E 's/^([^,]*),.*$/\1/')"
   SERVICE_STATUS="$(curl \
+                      ${curl_timeout_options} \
                       --silent \
                       --insecure \
                       --user "$BASIC_AUTH" \
@@ -69,124 +62,36 @@ ENDPOINT=""
 
   log "found endpoints: [ $(echo $ENDPOINTS | tr "\n" " ")]"
   for endpoint in $ENDPOINTS; do
-    log "checking whether port 22 is open on $endpoint ..."
-    if ! nc -z -v -w 3 "$endpoint" 22 &> /dev/null; then
-      log "error: port 22 on $endpoint is not open, can not use it"
+    log "checking whether port ${openvpn_port} is open on $endpoint ..."
+    if ! nc -z -v -w 3 "$endpoint" "${openvpn_port}" &> /dev/null; then
+      log "error: port ${openvpn_port} on $endpoint is not open, can not use it"
     else
-      log "port 22 on $endpoint is open, using it"
+      log "port ${openvpn_port} on $endpoint is open, using it"
       ENDPOINT="$endpoint"
       return
     fi
   done
 }
 
-function restart_vpn_shoot() {
-  log "deleting vpn-shoot container(s)..."
-  set +e
+function configure_tcp() {
+  echo $tcp_keepalive_time > /proc/sys/net/ipv4/tcp_keepalive_time
+  echo $tcp_keepalive_intvl > /proc/sys/net/ipv4/tcp_keepalive_intvl
+  echo $tcp_keepalive_probes > /proc/sys/net/ipv4/tcp_keepalive_probes
 
-  BASIC_AUTH="admin:$(cat /srv/auth/basic_auth.csv | sed -E 's/^([^,]*),.*$/\1/')"
-  RESPONSE="$(curl \
-                --silent \
-                --insecure \
-                --user "$BASIC_AUTH" \
-                --header "Accept: application/json" \
-                --request GET \
-                "https://$(get_host)/api/v1/namespaces/kube-system/pods?labelSelector=app=$service_name")"
-  POD_NAMES="$(echo $RESPONSE | jq -r '.items[].metadata.name')"
-
-  for name in "$POD_NAMES"; do
-    curl \
-      --silent \
-      --insecure \
-      --user "$BASIC_AUTH" \
-      --header "Accept: application/json" \
-      --request DELETE \
-      "https://$KUBE_APISERVER_SERVICE_HOST:$KUBE_APISERVER_SERVICE_PORT/api/v1/namespaces/kube-system/pods/$name"
-  done
-
-  set -e
-  log "deletion done"
+  echo $tcp_retries2 > /proc/sys/net/ipv4/tcp_retries2
 }
 
-while true; do
-  if ! ip addr | grep -e 'tun[0-9]*:' 1>/dev/null; then
-    log "no tunnel interface found..."
+configure_tcp
+
+while : ; do
+    # identify_endpoint may get an invalid endpoint, need
+    # to make sure openvpn is able to pick up the correct
+    # one once it has been registered
     identify_endpoint
-    if [[ ! -z "$ENDPOINT" ]]; then
-      if [[ -z "$MAIN_VPN_SEED" ]]; then
-        log "choosing randomly a tunnel device number"
-        TUN_DEVICE_NR="$(shuf -i 2-127 -n 1)"
-      else
-        log "using tunnel device number 1"
-        TUN_DEVICE_NR="1"
-      fi
-
-      if [[ $successful_ssh_connections_without_ping -ge 5 ]]; then
-        restart_vpn_shoot
-        successful_ssh_connections_without_ping=0
-        log "successful_ssh_connections_without_ping: $successful_ssh_connections_without_ping"
-      else
-        TUN_DEVICE="tun$TUN_DEVICE_NR"
-        LOCAL_IP="192.168.111.$TUN_DEVICE_NR"
-        REMOTE_IP="192.168.111.$(expr 127 + $TUN_DEVICE_NR)"
-        log "tun device: $TUN_DEVICE"
-        log "LOCAL_IP:   $LOCAL_IP"
-        log "REMOTE_IP:  $REMOTE_IP"
-
-        log "trying to establish the ssh connection..."
-        if ssh \
-          -w "$TUN_DEVICE_NR:$TUN_DEVICE_NR" \
-          -q \
-          -o "StrictHostKeyChecking=no" \
-          -o "ServerAliveInterval=30" \
-          -o "ServerAliveCountMax=5" \
-          -o "UserKnownHostsFile=/dev/null" \
-          -i "/var/vpn-seed/id_rsa" \
-          -f \
-          -N \
-          "$USER"@"$ENDPOINT"; then
-
-          # check whether tunnel device was created
-          if ip addr | grep -e "${TUN_DEVICE}:"; then
-            ip addr add local $LOCAL_IP peer $REMOTE_IP broadcast $LOCAL_IP dev $TUN_DEVICE
-            ip link set $TUN_DEVICE up
-            ip route add $service_network via $LOCAL_IP # service network
-            ip route add $pod_network     via $LOCAL_IP # pod network
-            ip route add $node_network    via $LOCAL_IP # host network
-            successful_ssh_connections_without_ping=$((successful_ssh_connections_without_ping+1))
-            log "success: ssh connection established, successful_ssh_connections_without_ping: $successful_ssh_connections_without_ping"
-          else
-            set +e
-            log "error: ssh connection established, but tun device not found - killing ssh connection"
-            kill -9 $(pgrep -fn "ssh -w $TUN_DEVICE_NR:$TUN_DEVICE_NR")
-            set -e
-          fi
-        else
-          log "error: ssh connection could not be established"
-        fi
-      fi
-    fi
-  else
-    TUN_DEVICE=$(ip addr | grep -e 'tun[0-9]*:' | sed -E 's/^.*(: (tun[0-9]*)\:).*/\2/')
-    TUN_DEVICE_NR=$(echo $TUN_DEVICE | sed -E 's/^tun([0-9]*)$/\1/')
-    REMOTE_IP="192.168.111.$(expr 127 + $TUN_DEVICE_NR)"
-
-    log "$TUN_DEVICE found. pinging peer to keep the connection up"
-    set +e
-    if ! ping $REMOTE_IP -w 5 -c 1 1>/dev/null; then
-      log "did not receive pong from peer after 5 seconds, trying further..."
-      let failed_pings++
-      if [[ $failed_pings -gt 3 ]]; then
-        log "did not receive a pong for 30 sec - killing ssh connection"
-        kill -9 $(pgrep -fn "ssh -w $TUN_DEVICE_NR:$TUN_DEVICE_NR")
-        failed_pings=0
-      fi
+    if [[ ! -z $ENDPOINT ]]; then
+        openvpn --remote ${ENDPOINT} --port ${openvpn_port} --config openvpn.config
     else
-      failed_pings=0
-      successful_ssh_connections_without_ping=0
-      log "pong from peer received successfully, successful_ssh_connections_without_ping: $successful_ssh_connections_without_ping"
+        log "No tunnel endpoint found"
     fi
-    set -e
-  fi
-  sleep 5
+    sleep 5
 done
